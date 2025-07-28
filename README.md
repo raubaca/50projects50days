d5a85afb1f4364df7ef86c993a6e90a3f6b05c0b
raubaca <raubaca@hotmail.com>
2025-07-28 18:04:50 -0400

    Progress Steps

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 Repository for projects in the course.
 
-| Day | Project         | Link |
-| :-: | --------------- | ---- |
-| 01  | Expanding Cards |      |
+| Day | Project         | Link                                                                |
+| :-: | --------------- | ------------------------------------------------------------------- |
+| 01  | Expanding Cards | [Link](https://raubaca.github.io/50projects50days/expanding-cards/) |
+| 02  | Progress Steps  | [Link](https://raubaca.github.io/50projects50days/progress-steps/)  |
---
+++ progress-steps/script.js
@@ -0,0 +1,26 @@
+const progress = document.getElementById('progress')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const circles = document.querySelectorAll('.circle')
+
+let currentStep = 0
+const MAX = progress.max
+
+next.addEventListener('click', (e) => update(e.target.id))
+prev.addEventListener('click', (e) => update(e.target.id))
+
+const update = (action) => {
+  action == 'next' ? currentStep++ : currentStep--
+  progress.value = currentStep
+
+  circles.forEach((circle, i) => {
+    if (i < currentStep + 1) {
+      circle.classList.add('active')
+    } else {
+      circle.classList.remove('active')
+    }
+  })
+
+  prev.disabled = currentStep === 0
+  next.disabled = currentStep === MAX
+}
